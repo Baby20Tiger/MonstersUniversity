57fb4e921e2c32a1252964fbbe989fc8a763b1b5
Baby20Tiger <92830899+Baby20Tiger@users.noreply.github.com>
2021-11-01 21:55:37 +0330

    jalase 3

---
+++ exercise/pr_mahdiyeh.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+//Mahdiyeh Heydari
+
+using namespace std;
+
+int main()
+{
+    int sizearaye;
+    cout<<"sizearaye ra vared konid"<<endl;
+    cin>>sizearaye;
+    int surati[sizearaye];
+    int sum = 0;
+    for(int i = 0; i < sizearaye; i++){
+        int n;
+        cout << "meghdar moteghaier ra vared konid" <<endl;
+        cin >> n;
+    if(n<0)
+    n=(-1)*n;
+    surati[i] = n;
+    sum += surati[i];
+    }
+    cout << "jam eshun : " << sum;
+    return 0;
+}
